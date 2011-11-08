3aafa3ba93b41186f3bb84e447ec57186ec85d03
marcoleonsarkis@gmail.com <marcoleonsarkis@gmail.com@3afbb233-a37b-5434-30fc-d761ebdd565f>
2011-11-08 20:42:12 +0000

    Restar valores menores de filas y columnas

--- MetodoAsignacion/src/Negocio/Administrador/Administrador.java
+++ MetodoAsignacion/src/Negocio/Administrador/Administrador.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Negocio.Administrador;
 
 /**
@@ -15,16 +14,62 @@ public class Administrador {
     }
 
     public void calcularSolOptima(int[][] matriz) {
+       matriz = restarValorMenorFilas(matriz);
+       matriz = restarValorMenorColumnas(matriz);
+       
+    }
+
+    public void imprimirMatriz(int[][] matriz) {
         String valoresMatriz = "";
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
-               valoresMatriz += matriz[i][j] +" - ";
+                valoresMatriz += matriz[i][j] + " - ";
             }
             valoresMatriz += "\n";
         }
         System.out.println(valoresMatriz);
     }
 
+    public int[][] restarValorMenorFilas(int[][] matriz) {
+        int[] valoresMenores = new int[matriz.length];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (j == 0) {
+                    valoresMenores[i] = matriz[i][j];
+                } else {
+                    if (matriz[i][j] < valoresMenores[i]) {
+                        valoresMenores[i] = matriz[i][j];
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = matriz[i][j] - valoresMenores[i];
+            }
+        }
+        return matriz;
+    }
 
-
+    public int [][] restarValorMenorColumnas(int[][] matriz){
+        int[] valoresMenores = new int[matriz[0].length];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (j == 0) {
+                    valoresMenores[i] = matriz[j][i];
+                } else {
+                    if (matriz[j][i] < valoresMenores[i]) {
+                        valoresMenores[i] = matriz[j][i];
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[j][i] = matriz[j][i] - valoresMenores[i];
+            }
+        }
+        return matriz;
+    }
+    
 }
